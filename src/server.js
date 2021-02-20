f42aca7880193f34592ddd05c6e902ae29c3fe82
Patrick Almeida Silva <palmeids@everis.com>
2021-02-19 23:09:58 -0300

    create data list passenger

--- src/server.js
+++ src/server.js
@@ -1,9 +1,26 @@
 const express = require('express');
-
 const app = express();
 
+const passengers = [
+  {
+    name: 'Joyce',
+    flightNumber: 7859,
+    time: '18h00'
+  },
+  {
+    name: 'Brock',
+    flightNumber: 7859,
+    time: '18h00'
+  },
+  {
+    name: 'Eve',
+    flightNumber: 7859,
+    time: '18h00'
+  },
+]
+
 app.get('/', (request, response) => {
-  return response.send('ok');
+  return response.send(passengers);
 });
 
 app.listen(3000);
